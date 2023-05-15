7cef7336e7ff7e70905db46f822601ef7d704711
spacefishdev <ultragamesalot@gmail.com>
2023-05-15 14:48:57 +0200

    Removed do.sh

--- do.sh
+++
@@ -1,9 +0,0 @@
-RES="1920 822 60" && \
-DISP=$(xrandr | grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/") && \
-MODELINE=$(cvt $(echo $RES) | grep -e "Modeline [^(]" | sed -r 's/.*Modeline (.*)/\1/') && \
-MODERES=$(echo $MODELINE | grep -o -P '(?<=").*(?=")') && \
-cat > ~/.xprofile << _EOF
-#!/bin/sh
-xrandr --newmode $MODELINE
-xrandr --addmode $DISP $MODERES
-_EOF
